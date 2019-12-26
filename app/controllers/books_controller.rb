4b080f3591d526f6909d478b31ab644e3f7f4071
João Cardoso <joaocardoso@a88-157-93-105.static.cpe.netcabo.pt>
2019-12-26 14:46:13 +0000

    Added expection handler, book controller and routes changes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,41 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show, :update, :destroy]
+
+  def index
+    books = Book.all
+    json_response(books)
+  end
+
+  def create
+    book = Book.create(book_param)
+    json_response(book)
+  end
+
+  def show
+    json_response(@book)
+  end
+
+  def update
+    if @book.update(book_param)
+      json_response(@book)
+    else
+      json_response({message: @book.errors }, :bad_request)
+    end
+  end
+
+  def destroy
+    if @book.destroy
+      json_response(@destroy)
+    else
+      json_response({message: @book.errors})
+    end
+  end
+
+  private
+  def book_param
+    params.require(:book).permit(:name, :bio, :author_id)
+  end
+  def set_book
+    @book = Book.find(params[:id])
+  end
 end
