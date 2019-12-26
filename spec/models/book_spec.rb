11bfe6c6c0440dab3519b617b11f33be112f4de6
Hextez <joao.r.d.cardoso@gmail.com>
2019-12-26 14:24:22 +0000

    Added RSpec test to models (#2)

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  context "Model structure is correct" do
+    it "has its required columns" do
+      expect(subject).to have_db_column(:name)
+    end
+
+    it "has its associations" do
+      expect(subject).to belong_to(:author)
+    end
+  end
+  
+  context "Model attributes" do
+    let(:author) { Author.create(name: "Joao") }
+
+    it "is valid with valid attributes" do
+      book = Book.new(name: "BookOne", author: author)
+      book.valid?
+      expect(book.errors[:name]).not_to include("can't be blank")
+    end
+    it "is not valid without a author" do
+      book = Book.new(name: "BookOne", author: nil)
+      book.valid?
+      expect(book.errors[:author]).to include("can't be blank")
+    end
+    it "is not valid without a author" do
+      book = Book.new(name: nil, author: author)
+      book.valid?
+      expect(book.errors[:name]).to include("can't be blank")
+    end
+  end
+
 end
--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Client, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  context "Model structure is correct" do
+    it "has its required columns" do
+      expect(subject).to have_db_column(:username)
+      expect(subject).to have_db_column(:email)
+    end
+  end
+
+  context "Model valid attributes" do
+    it "has valid attributes" do
+      client = Client.new(username: "Joao", email: "asd@sadas.com")
+      expect(client).to be_valid
+    end
+
+    it "has no name attribute" do
+      client = Client.new(username: nil, email: "asd@sadas.com")
+      expect(client).not_to be_valid
+    end
+    it "has no email attribute" do
+      client = Client.new(username: "JOAO", email: nil)
+      expect(client).not_to be_valid
+    end
+    
+    it "has valid no attributes" do
+      client = Client.new(username: nil, email: nil)
+      expect(client).not_to be_valid
+    end
+
+    it "has invalid email" do
+      client = Client.new(username: "JOAO", email: "asdsadas.com")
+      client.valid?
+      expect(client.errors[:email]).to include("Invalid email format")
+    end
+  end
 end
